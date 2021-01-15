c2f6b9e64365ad885de7a5694e3df0173d5a5997
Alex Devero <deveroalex@gmail.com>
2021-01-15 13:54:17 +0100

    Add route for deleting episode

--- server/routes/episodes-route.js
+++ server/routes/episodes-route.js
@@ -51,3 +51,14 @@ router.patch('/update', async (req, res) => {
   }
 })
 
+router.delete('/delete/:episode_id', async (req, res) => {
+  try {
+    const episode = await Episode.remove({ id: req.params.episode_id })
+
+    res.json({ message: 'Episode deleted.' })
+  } catch (err) {
+    res.status(500).json({
+      message: err.message
+    })
+  }
+})
