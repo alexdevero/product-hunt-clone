761468f8479efc19aac7c1ab54d31d4c6674a8a8
Alex Devero <deveroalex@gmail.com>
2021-01-15 13:53:49 +0100

    Add route for specific episode

--- server/routes/episodes-route.js
+++ server/routes/episodes-route.js
@@ -16,3 +16,19 @@ router.get('/', async (req, res) => {
   }
 })
 
+router.get('/:episode_id', async (req, res) => {
+  try {
+    const episode = await Episode.find({ id: req.params.episode_id })
+
+    if (episode !== null || (Array.isArray(episode) && episode.length !== 0)) {
+      res.json(episode)
+    } else {
+      res.status(404).json({ message: 'Couldn\'t find specified episode.' })
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: err.message
+    })
+  }
+})
+
