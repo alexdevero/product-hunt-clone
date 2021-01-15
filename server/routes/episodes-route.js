92bdff1ffb7817681a8bb89ec8527bad823f66ed
Alex Devero <deveroalex@gmail.com>
2021-01-15 13:53:34 +0100

    Add route for all episodes

--- server/routes/episodes-route.js
+++ server/routes/episodes-route.js
@@ -1,1 +1,18 @@
 const express = require('express')
+
+const Episode = require('./../models/episode-model')
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+  try {
+    const episodes = await Episode.find()
+
+    res.json(episodes)
+  } catch (err) {
+    res.status(500).json({
+      message: err.message
+    })
+  }
+})
+
