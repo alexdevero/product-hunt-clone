8e2a54a28ffc3ba18d74f698d521ba852bb77f05
Alex Devero <deveroalex@gmail.com>
2021-01-18 12:50:32 +0100

    Rewrite episodes routes to promises

--- server/routes/episodes-route.js
+++ server/routes/episodes-route.js
@@ -5,35 +5,31 @@ const Episode = require('./../models/episode-model')
 const router = express.Router()
 
 router.get('/', async (req, res) => {
-  try {
-    const episodes = await Episode.find()
-
-    res.json(episodes)
-  } catch (err) {
-    res.status(500).json({
-      message: err.message
-    })
-  }
+  const episodes = await Episode.find((err, data) => {
+    if (err) {
+      res.status(500).json({
+        message: err.message
+      })
+    } else {
+      res.json(data)
+    }
+  })
 })
 
 router.get('/:episode_id', async (req, res) => {
-  try {
-    const episode = await Episode.find({ _id: req.params.episode_id })
-
-    if (episode !== null || (Array.isArray(episode) && episode.length !== 0)) {
-      res.json(episode)
+  const episode = await Episode.find({ _id: req.params.episode_id }, (err, data) => {
+    if (err) {
+      res.status(500).json({
+        message: err.message
+      })
     } else {
-      res.status(404).json({ message: 'Couldn\'t find specified episode.' })
+      res.json(data)
     }
-  } catch (err) {
-    res.status(500).json({
-      message: err.message
-    })
-  }
+  })
 })
 
 router.post('/create', async (req, res) => {
-  try {
+  if (req.body.title !== null && req.body.url !== null && req.body.description !== null) {
     const episode = new Episode({
       title: req.body.title,
       url: req.body.url,
@@ -42,50 +38,38 @@ router.post('/create', async (req, res) => {
 
     const newEpisode = await episode.save().then(() => {
       res.json({ message: 'Episode created.' })
-    })
-  } catch (err) {
-    res.status(500).json({
-      message: err.message
+    }).catch(err => {
+      res.status(500).json({
+        message: err.message
+      })
     })
   }
 })
 
 router.patch('/update', async (req, res) => {
-  try {
-    if (req.body._id !== null) {
-      const updateEpisode = { ...req.body }
+  if (req.body._id !== null) {
+    const updateEpisode = { ...req.body }
 
-      const episode = Episode.findOneAndUpdate({ _id: req.body._id }, updateEpisode, { useFindAndModify: false }, (err) => {
-        if (err) {
-          res.status(500).json({ message: err.message })
-        } else {
-          res.json({ message: 'Episode updated.' })
-        }
-      })
-    } else {
-      res.status(500).json({ message: 'Please specify episode id.' })
-    }
-  } catch (err) {
-    res.status(500).json({
-      message: err.message
+    const episode = await Episode.findOneAndUpdate({ _id: req.body._id }, updateEpisode, { useFindAndModify: false }, (err) => {
+      if (err) {
+        res.status(500).json({ message: err.message })
+      } else {
+        res.json({ message: 'Episode updated.' })
+      }
     })
+  } else {
+    res.status(500).json({ message: 'Please specify episode id.' })
   }
 })
 
 router.delete('/delete', async (req, res) => {
-  try {
-    if (req.body._id !== null) {
-      const episode = await Episode.remove({ _id: req.body._id }, (err) => {
-        if (err) {
-          res.status(500).json({ message: err.message })
-        } else {
-          res.json({ message: 'Episode deleted.' })
-        }
-      })
-    }
-  } catch (err) {
-    res.status(500).json({
-      message: err.message
+  if (req.body._id !== null) {
+    const episode = await Episode.deleteOne({ _id: req.body._id }, (err) => {
+      if (err) {
+        res.status(500).json({ message: err.message })
+      } else {
+        res.json({ message: 'Episode deleted.' })
+      }
     })
   }
 })
