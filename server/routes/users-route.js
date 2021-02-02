354703270f466bc23477eee6c1869912b84bd747
Alex Devero <deveroalex@gmail.com>
2021-02-02 16:09:38 +0100

    Add route to get one user

--- server/routes/users-route.js
+++ server/routes/users-route.js
@@ -12,3 +12,14 @@ router.get('/', async (req, res) => {
     }
   })
 })
+
+router.get('/:user_id', async (req, res) => {
+  const user = await User.find({ _id: req.params.user_id }, (err, data) => {
+    if (err) {
+      res.status(500).json({ message: err.message })
+    } else {
+      res.status(200).json(data)
+    }
+  })
+})
+
