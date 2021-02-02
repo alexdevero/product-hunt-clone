aa80914ca7561c2a4434e0a78944ea21b426a464
Alex Devero <deveroalex@gmail.com>
2021-02-02 16:09:23 +0100

    Add route to get all users

--- server/routes/users-route.js
+++ server/routes/users-route.js
@@ -1,1 +1,14 @@
 const express = require('express')
+const User = require('./../models/user-model')
+
+const router = express.Router()
+
+router.get('/', async (req, res) => {
+  const user = await User.find((err, data) => {
+    if (err) {
+      res.status(500).json({ message: err.message })
+    } else {
+      res.status(200).json(data)
+    }
+  })
+})
